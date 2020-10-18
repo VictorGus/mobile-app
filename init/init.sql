6f02c4b7bef2fff3f7da8b87cf687739915ddb7e
VictorGus <retaow@gmail.com>
2020-10-18 17:19:48 +0300

    init mobile app

--- init/init.sql
+++ init/init.sql
@@ -1,7 +1,10 @@
-create table if not exists public_user (id text primary key not null, ts timestamp with time zone DEFAULT CURRENT_TIMESTAMP, resource jsonb);
-
 create table if not exists settings (id text primary key not null, ts timestamp with time zone DEFAULT CURRENT_TIMESTAMP, resource jsonb);
 
 create table if not exists condition (id text primary key not null, ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, resource jsonb);
+
+create table if not exists public_user (id text primary key not null, ts timestamp with time zone DEFAULT CURRENT_TIMESTAMP, settings_id text, condition_id text, CONSTRAINT fk_condition FOREIGN KEY(condition_id) REFERENCES settings(id) ON DELETE SET NULL, CONSTRAINT fk_settings FOREIGN KEY(settings_id) REFERENCES settings(id) ON DELETE SET NULL, resource jsonb);
+
+
+create table if not exists notification (id text primary key not null, ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, user_id text, CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES public_user(id) ON DELETE SET NULL, resource jsonb);
 
 SELECT 'CREATE DATABASE fortest TEMPLATE mobile-db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'fortest')\gexec
--- temp.sql
+++ temp.sql
@@ -15,7 +15,21 @@ create table if not exists settings (id text primary key not null,
 ----
 create table if not exists condition (id text primary key not null,
                                       ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                                      user_id text,
+                                      CONSTRAINT fk_user
+                                      FOREIGN KEY(user_id)
+                                      REFERENCES public_user(id)
+                                      ON DELETE SET NULL,
                                       resource jsonb);
+----
+create table if not exists notification (id text primary key not null,
+ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+user_id text,
+CONSTRAINT fk_user
+FOREIGN KEY(user_id)
+REFERENCES public_user(id)
+ON DELETE SET NULL,
+resource jsonb);
 ----
 insert into public_user (id, settings_id) values ('user-1', 'set-1')
 ----
@@ -24,4 +38,6 @@ insert into settings (id) values ('set-1')
 select * from public_user p left join settings s on p.settings_id = s.id
 ----
 update public_user set resource = '{"name":{"family":"Test","given":"Test"},"email":"test@gmail.com","username":"test","age":"45","active":true,"password":"encrypted"}'::jsonb;
+----
+drop table condition
 ----
