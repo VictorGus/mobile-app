e46f143a1c6dfa0ce42269c6312d45fc095a7f2b
VictorGus <retaow@gmail.com>
2020-11-20 16:52:34 +0300

    add notification create form

--- mobile/MobileApp/Notification.js
+++ mobile/MobileApp/Notification.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View, StyleSheet, Text, ScrollView, TouchableOpacity} from 'react-native';
-import { Card, Button } from 'react-native-elements';
+import { View, StyleSheet, Text, ScrollView, TouchableOpacity, Modal, Keyboard} from 'react-native';
+import { Picker } from '@react-native-picker/picker'
+import { Card, Button, Input} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome'; 
 import IconFA from 'react-native-vector-icons/FontAwesome5'; 
 import FileSystem from 'react-native-fs'
 import SegmentedControlTab from 'react-native-segmented-control-tab'
+import DateTimePicker from 'react-native-modal-datetime-picker';
 
 const demoData = [
     {
@@ -56,74 +58,189 @@ function NotificationIcon(props) {
 }
 
 function testG() {
+    alert('You tapped the button!')
     return 1;
 }
 
 const UpcomingNotifications = () => (
-    <ScrollView style={{
-        margin: 10,
-        marginTop: 5,
-        marginBottom: 15
-    }}>
-        {
-            demoData.map((el, i) => (
-                <Card key={i}>
-                    <View style={{
-                        flexDirection: 'row',
-                        flexGrow: 2,
-                        marginBottom: 10,
-                        alignContent: 'space-between'
-                    }}>
-                        <Text>
-                            <NotificationIcon category={el.category} />
-                        </Text>
-                        <View style={{
-                            marginLeft: 5,
-                            marginTop: 5,
-                            flex: 1
-                        }}>
-                            <Text style={{
-                                fontWeight: 'bold',
-                                fontSize: 19
-                            }}>
-                                {"Today 16:35"}
-                            </Text>
-                        </View>
-                        <TouchableOpacity onPress={testG}>
-                            <Icon style={{ marginRight: 10 }} name="check-circle" size={36} color="green" />
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={testG}>
-                            <Icon name="times-circle" size={36} color="red" />
-                        </TouchableOpacity>
-                    </View>
-                    <Card.Divider />
-                    <View style={{
-                        flexDirection: 'row'
-                    }}>
-                        <View>
-                            <Text style={{
-                                fontSize: 24,
-                            }}>
-                                {el.message}
-                            </Text>
-                        </View>
-                    </View>
-                </Card>
-            ))
-        }
-    </ScrollView>
-); 
-
-const CreatedNotifications = () => (
     <View style={{
-        alignItems: 'center',
-        marginBottom: 15
+        flex: 1
     }}>
-        <TouchableOpacity onPress={testG}>
-            <Icon name="plus" size={49} color="#2396d9" />
-        </TouchableOpacity>
+        <ScrollView style={{
+            margin: 10,
+            marginTop: 5,
+            marginBottom: 15
+        }}>
+            {
+                demoData.map((el, i) => (
+                    <Card key={i}>
+                        <View style={{
+                            flexDirection: 'row',
+                            flexGrow: 2,
+                            marginBottom: 10,
+                            alignContent: 'space-between'
+                        }}>
+                            <Text>
+                                <NotificationIcon category={el.category} />
+                            </Text>
+                            <View style={{
+                                marginLeft: 5,
+                                marginTop: 5,
+                                flex: 1
+                            }}>
+                                <Text style={{
+                                    fontWeight: 'bold',
+                                    fontSize: 19
+                                }}>
+                                    {"Today 16:35"}
+                                </Text>
+                            </View>
+                            <TouchableOpacity onPress={testG}>
+                                <Icon style={{ marginRight: 10 }} name="check-circle" size={36} color="green" />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={testG}>
+                                <Icon name="times-circle" size={36} color="red" />
+                            </TouchableOpacity>
+                        </View>
+                        <Card.Divider />
+                        <View style={{
+                            flexDirection: 'row'
+                        }}>
+                            <View>
+                                <Text style={{
+                                    fontSize: 24,
+                                }}>
+                                    {el.message}
+                                </Text>
+                            </View>
+                        </View>
+                    </Card>
+                ))
+            }
+        </ScrollView>
     </View>
 );
+
+const CreatedNotifications = () => {
+    const [modalVisible, setModalVisible] = React.useState(false);
+    const [isDatePickerVisible, setDatePickerVisibility] = React.useState(false);
+    const showDatePicker = () => {
+        setDatePickerVisibility(true);
+      };
+
+    return (
+        <View style={{ flex: 1 }}>
+            <ScrollView style={{
+                margin: 10,
+                marginTop: 5,
+                marginBottom: 15
+            }}>
+                {
+                    demoData.map((el, i) => (
+                        <Card key={i}>
+                            <View style={{
+                                position: 'relative',
+                                flexDirection: 'row',
+                                flexGrow: 2,
+                                marginBottom: 10,
+                                alignContent: 'space-between'
+                            }}>
+                                <Text>
+                                    <NotificationIcon category={el.category} />
+                                </Text>
+                                <View style={{
+                                    marginLeft: 5,
+                                    marginTop: 5,
+                                    flex: 1,
+                                    flexDirection: 'row'
+                                }}>
+                                    <Text style={{
+                                        fontWeight: 'bold',
+                                        fontSize: 19
+                                    }}>
+                                        {"Today 16:35"}
+                                    </Text>
+                                    <Icon style={{
+                                        marginRight: 10,
+                                        marginLeft: 2
+                                    }} name="repeat" size={14} color="#2396d9" />
+                                </View>
+                                <TouchableOpacity onPress={testG}>
+                                    <Icon style={{ marginRight: 0 }} name="pencil" size={36} color="#2396d9" />
+                                </TouchableOpacity>
+                                {/* <TouchableOpacity style={{
+                                position: 'absolute',
+                                right: -20,
+                                zIndex: 999,
+                                bottom: 35
+                            }} onPress={testG}>
+                                <Icon onPress={() => { alert('You tapped the button!'); }} name="close" size={24} color="red" />
+                            </TouchableOpacity> */}
+                            </View>
+                            <Card.Divider />
+                            <View style={{
+                                flexDirection: 'row'
+                            }}>
+                                <View>
+                                    <Text style={{
+                                        fontSize: 24,
+                                    }}>
+                                        {el.message}
+                                    </Text>
+                                </View>
+                            </View>
+                        </Card>
+                    ))
+                }
+            </ScrollView>
+                <Modal animationType='slide' transparent={true} visible={modalVisible}>
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text>
+                            {"Action"}
+                        </Text>
+                        <Input
+                            style={styles.textInput}
+                            placeholder="Enter action"
+                            maxLength={20}
+                            onBlur={Keyboard.dismiss}
+                        />
+                        <Text>
+                            {"Category"}
+                        </Text>
+                        <Picker style={styles.textInput} label={"Category"}>
+                            <Picker.Item label="Nutrition order" value="nutrition-order" />
+                            <Picker.Item label="Pills" value="pills" />
+                            <Picker.Item label="Water consumption" value="water" />
+                            <Picker.Item label="Walks" value="walking" />
+                            <Picker.Item label="Acitivities" value="activities" />
+                            <Picker.Item label="Medical services" value="medical-services" />
+                        </Picker>
+                        <Button title="Show Date Picker" onPress={showDatePicker} />
+                        <DateTimePicker
+                            isVisible={isDatePickerVisible}
+                            testID="dateTimePicker"
+                            value={Date.now()}
+                            onConfirm={()=>{}}
+                            mode={"datetime"}
+                            is24Hour={true}
+                            display="default"
+                            // onChange={onChange}
+                        />
+                    </View>
+                </View>
+            </Modal>
+            <View style={{
+                alignItems: 'center',
+                marginBottom: 10
+            }}>
+                <TouchableOpacity onPress={() => { setModalVisible(!modalVisible) }}>
+                    <Icon name="plus" size={49} color="#2396d9" />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
 
 const NotificationScreen = () => {
     const [selectedIndex, handleIndexChange] = React.useState(0);
@@ -144,5 +261,37 @@ const NotificationScreen = () => {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+      },
+      modalView: {
+        margin: 20,
+        width: 300,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+      },
+      textInput: {
+        borderColor: '#CCCCCC',
+        borderBottomWidth: 1,
+        width: "100%",
+        height: 50,
+        fontSize: 18,
+      }
+})
 
 export default NotificationScreen;
